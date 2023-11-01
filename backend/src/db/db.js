1df3dba7f6e4409df0acdee15f84135c2e6fb328
patrickqq1 <jrpatrick112@gmail.com>
2023-11-01 13:32:05 -0300

    ajuste

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -3,9 +3,9 @@ import knex from 'knex'
 export const db = knex({
     client: 'mysql2',
     connection: {
-        host: '193.203.174.253',
-        user: 'patrick',
-        password: 'abc@123',
+        host: '',
+        user: '',
+        password: '',
         database: 'controlefinanc',
     }
 })
