800fba5c2958dcd1a3ffc63d9f565e58ae142177
patrickqq1 <jrpatrick112@gmail.com>
2023-11-08 09:18:21 -0300

    adjust

--- frontend/src/api/api.jsx
+++ frontend/src/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from "js-cookie"
 
 export const api = axios.create({
-    baseURL: ''
+    baseURL: process.env.URL_API
 })
 
 api.interceptors.request.use((config) => {
